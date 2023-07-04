4c6465039453b9bede15a51194ef1b0a766d6de2
은지 <yeochinlebelomgeole@gmail.com>
2023-07-05 08:51:18 +0900

    [create] Rank

--- src/components/rank/index.jsx
+++ src/components/rank/index.jsx
@@ -1,8 +1,41 @@
 import { styled } from "styled-components";
 
 export default function Rank() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Box>
+        <Box1></Box1>
+        <Box2></Box2>
+        <Box3></Box3>
+      </Box>
+    </Container>
+  );
 }
+
+const Box3 = styled.div`
+  width: 1400px;
+  height: 100px;
+  background-color: white;
+  border-radius: 20px;
+`;
+
+const Box2 = styled.div`
+  width: 1400px;
+  height: 100px;
+  background-color: white;
+  border-radius: 20px;
+`;
+
+const Box1 = styled.div`
+  width: 1400px;
+  height: 100px;
+  background-color: white;
+  border-radius: 20px;
+`;
+
+const Box = styled.div`
+  gap: 20px;
+`;
 
 const Container = styled.div`
   background-color: #ff9128;
