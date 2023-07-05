2b78140c784a498001ced0d7ed4cf5d4fe777d63
soyeon <hippochl07067@gmail.com>
2023-07-05 17:04:56 +0900

    [UPDATE] imageID

--- src/components/result/index.jsx
+++ src/components/result/index.jsx
@@ -1,35 +1,126 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { styled } from "styled-components";
 import { Link } from "react-router-dom";
 
 const Result = () => {
   const [result, setResult] = useState({
-    resultID: 0,
-    hobby: "",
-    content: "",
-    img: "",
+    resultID: 6,
+    hobby: "독서",
+    content: "책을 읽어 마음의 소양을 기릅니다",
+    img: "/imgs/togetherMountain.png",
   });
-  useEffect(() => {
-    let obj = result;
+  const getImage = () => {
     switch (result.resultID) {
-      case 0:
-        obj.img = "/imgs/art.jpeg";
-        setResult(obj);
-        break;
       case 1:
-        obj.img = "/imgs/swim.png";
-        setResult(obj);
-        break;
+        return "/imgs/swim.png";
       case 2:
-        obj.img = "/imgs/skateBoard.jpg";
-        setResult(obj);
-        break;
+        return "/imgs/skateBoard.jpg";
       case 3:
-        obj.img = "/imgs/bike.png";
-        setResult(obj);
-        break;
+        return "/imgs/bike.png";
+
+      case 4:
+        return "/imgs/jogging.jpg";
+
+      case 5:
+        return "/imgs/swim.png";
+
+      case 6:
+        return "/imgs/yoga.png";
+
+      case 7:
+        return "/imgs/mountain.jpg";
+
+      case 8:
+        return "/imgs/walking.jpg";
+
+      case 9:
+        return "/imgs/swim.png";
+
+      case 10:
+        return "/imgs/readingBook.png";
+
+      case 11:
+        return "/imgs/puzzle.jpg";
+
+      case 12:
+        return "/imgs/drawing.avif";
+
+      case 13:
+        return "/imgs/watchMovie.jpg";
+
+      case 14:
+        return "/imgs/jasu.jpg";
+
+      case 15:
+        return "/imgs/gongYe.jpg";
+
+      case 16:
+        return "/imgs/drama.jpg";
+
+      case 17:
+        return "/imgs/ddegaejil.avif";
+
+      case 18:
+        return "/imgs/listenMusic.png";
+
+      case 19:
+        return "/imgs/soccer.png";
+
+      case 20:
+        return "/imgs/baseball.png";
+
+      case 21:
+        return "/imgs/volleyball.png";
+
+      case 22:
+        return "/imgs/football.png";
+
+      case 23:
+        return "/imgs/tableTennis.png";
+
+      case 24:
+        return "/imgs/soccer.png";
+
+      case 25:
+        return "/imgs/danggu.png";
+
+      case 26:
+        return "/imgs/maraton.png";
+
+      case 27:
+        return "/imgs/togetherMountain.png";
+
+      case 28:
+        return "/imgs/boardgame.png";
+
+      case 29:
+        return "/imgs/watchMusical.png";
+
+      case 30:
+        return "/imgs/shopping.png";
+
+      case 31:
+        return "/imgs/hwatu.png";
+
+      case 32:
+        return "/imgs/watchSprots.png";
+
+      case 33:
+        return "/imgs/shopping.png";
+
+      case 34:
+        return "/imgs/baduk.png";
+
+      case 35:
+        return "/imgs/hwatu.png";
+
+      case 36:
+        return "/imgs/janggi.png";
+
+      default:
+        return "/imgs/art.jpeg";
     }
-  }, [result]);
+  };
 
   return (
     <Container>
